374f1e26ab614de829b56a5d632e6ea26e678580
Michael Snoyman <michael@snoyman.com>
2011-12-07 18:55:14 +0200

    try

--- Data/Enumerator/Exception.hs
+++ Data/Enumerator/Exception.hs
@@ -1,14 +1,23 @@
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE CPP #-}
 module Data.Enumerator.Exception
     ( catch
+    , try
     ) where
 
+#if MC03
 import Control.Monad.Trans.Control (MonadBaseControl)
 import qualified Control.Exception.Lifted as E
+#define MBCIO MonadBaseControl IO
+#else
+import Control.Monad.IO.Control (MonadControlIO)
+import qualified Control.Exception.Control as E
+#define MBCIO MonadControlIO
+#endif
 import Data.Enumerator (Iteratee (..), Step (..))
 import Prelude hiding (catch)
 
-catch :: (E.Exception e, MonadBaseControl IO m)
+catch :: (E.Exception e, MBCIO m)
       => Iteratee a m b
       -> (e -> Iteratee a m b)
       -> Iteratee a m b
@@ -18,3 +27,8 @@ catch (Iteratee mstep) f = Iteratee $ do
         Continue k -> Continue $ \s -> catch (k s) f
         Yield b s -> Yield b s
         Error e -> Error e
+
+try :: (E.Exception e, MBCIO m)
+    => Iteratee a m b
+    -> Iteratee a m (Either e b)
+try i = catch (fmap Right i) (return . Left)
--- test/main.hs
+++ test/main.hs
@@ -1,13 +1,23 @@
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE Rank2Types #-}
 import Test.Hspec.Monadic
 import Test.Hspec.HUnit
 import Control.Exception (SomeException)
 import qualified Data.Enumerator as E
 import qualified Data.Enumerator.List as EL
 import qualified Data.Enumerator.Exception as EE
-import Control.Monad.Trans.Control (MonadBaseControl)
 import Control.Monad.Trans.Reader (runReaderT)
 import Control.Monad.Trans.Writer (runWriterT)
+import Control.Monad ((=<<))
+
+#if MIN_VERSION_monad_control(0, 3, 0)
+import Control.Monad.Trans.Control (MonadBaseControl)
+#define MBCIO MonadBaseControl IO
+#else
+import Control.Monad.IO.Control (MonadControlIO)
+#define MBCIO MonadControlIO
+#endif
 
 main :: IO ()
 main = hspecX $ do
@@ -15,14 +25,27 @@ main = hspecX $ do
         it "IO" $ caseCatch id
         it "ReaderT" $ caseCatch $ flip runReaderT ()
         it "WriterT" $ caseCatch $ fmap fst' . runWriterT
+    describe "try" $ do
+        it "IO" $ caseTry id
+        it "ReaderT" $ caseTry $ flip runReaderT ()
+        it "WriterT" $ caseTry $ fmap fst' . runWriterT
 
-fst' :: ((), ()) -> ()
-fst' ((), ()) = ()
+fst' :: (a, ()) -> a
+fst' (a, ()) = a
 
-caseCatch :: MonadBaseControl IO m => (m () -> IO ()) -> IO ()
+caseCatch :: MBCIO m => (m () -> IO ()) -> IO ()
 caseCatch unwrap = unwrap $ E.run_ $ E.enumList 8 [1..1000 :: Int] E.$$ flip EE.catch ignorer $ do
     _ <- EL.consume
     error "foo"
   where
     ignorer :: Monad m => SomeException -> E.Iteratee a m ()
     ignorer _ = return ()
+
+caseTry :: MBCIO m => (forall a. m a -> IO a) -> IO ()
+caseTry unwrap = check =<< (unwrap $ E.run_ $ E.enumList 8 [1..1000 :: Int] E.$$ EE.try $ do
+    _ <- EL.consume
+    error "foo")
+  where
+    check :: Either SomeException () -> IO ()
+    check (Left _) = return ()
+    check (Right ()) = error "There should have been an exception caught"
